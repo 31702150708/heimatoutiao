37edb798f99a7e4bec035bea7230f828fef91df0
她和影流都是劫 <1275149846@qq.com>
2020-03-28 18:15:32 +0800

    heimaotoutiao

---
+++ heimatoutiao/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+// @import url("");
+@import url("../node_modules/vant/lib/index.css");
+// 公共样式
+* {
+  padding: 0;
+  margin: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+a:hover {
+  color: inherit;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+i,
+em {
+  font-style: normal;
+}
+</style>
---
+++ heimatoutiao/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+// 路由配置
+const routes = [
+  {
+    // 登录页
+    path: "/login",
+    // 除了首页之外的页面都可以用懒加载,@符号代表的src这个目录
+    // .vue这个扩展名可以忽略
+    component: () => import("@/views/Login")
+  },
+  {
+    // 注册页
+    path: "/register",
+    component: () => import("@/views/Register")
+  },
+  {
+    // 个人中心页
+    path: "/personal",
+    component: () => import("@/views/Personal")
+  },
+  {
+    // 编辑页
+    path: "/editProfile",
+    component: () => import("@/views/EditProfile")
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
